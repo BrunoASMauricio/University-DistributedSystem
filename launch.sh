d49525dd4fe124dd64b2365f9cd785ad621282bd
BrunoASMauricio <brunoasmauricio@gmail.com>
2020-12-16 16:43:44 +0000

    Added launch script

---
+++ launch.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Need ammount of nodes as argument"
+	exit 1
+fi
+
+re='^[0-9]+$'
+nodes=$1
+
+if ! [[ $1 =~ $re ]] ; then
+   echo "error: \"$nodes\" is not a number"
+   exit 1
+fi
+
+if [ $nodes -le 0 ]
+then
+	echo "Node ammount must be greater than 0"
+	exit 1
+fi
+
+nodes=$(($nodes + 1))
+
+
+# Sub interface route
+sudo route add \-net 127.0.0.0 netmask 255.255.255.0 dev lo
+# Multicast route
+sudo route add \-net 239.255.0.0 netmask 255.255.255.0 dev lo
+
+# Set interface and respective sub-interfaces for multicast
+sudo ifconfig lo multicast
+
+for i in $( eval echo {2..$nodes} )
+do
+	sudo ifconfig lo:$i 127.0.0.$i netmask 255.0.0.0 up
+	sudo ifconfig lo:$i multicast
+done
+
+cd paxos_node
+make
+
+# turn nodes on
+for i in $( eval echo {2..$nodes} )
+do
+	./paxos_node i &
+done
+
+cd ../paxos_client
+make
+
+# turn client on
+./paxos_client
+
+
+
+
+# clean
+
+# Sub interface route
+sudo route del \-net 127.0.0.0 netmask 255.255.255.0 dev lo
+# Multicast route
+sudo route del \-net 239.255.0.0 netmask 255.255.255.0 dev lo
+
+for i in $( eval echo {2..$nodes} )
+do
+	sudo ifconfig lo:$i down
+done
+
+
+
+
+
