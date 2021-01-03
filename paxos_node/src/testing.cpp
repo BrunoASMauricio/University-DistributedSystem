621d272325109bdf7a9fafe3454977e8e206b891
BrunoASMauricio <brunoasmauricio@gmail.com>
2021-01-03 12:26:12 +0000

    Added basic file write function

--- paxos_node/src/testing.cpp
+++ paxos_node/src/testing.cpp
@@ -8,3 +8,16 @@ bool testLostPacket(byte* buf, uint16_t nbytes){
 	}
 	return false;
 }
+void writeToFile(char* string, int id){
+	static FILE* fp = NULL;
+	char filename[9];
+	if(!fp){
+		snprintf(filename, 9, "./node_%d", id);
+		fp = fopen(filename, "a");
+		if(!fp){
+			perror("fopen");
+			return;
+		}
+	}
+	fwrite(string, sizeof(char), strlen(string), fp);
+}
